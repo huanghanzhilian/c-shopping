24d84f0975d3713f0fd16211cfd3b1837921c1fe
huanghanzhilian <1319639755@qq.com>
2023-11-27 10:43:41 +0800

    feat: add toast config

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,7 +1,16 @@
+"use client"
+
 import StoreProvider from 'app/StoreProvider'
+import { Toaster } from "react-hot-toast";
 
 export default function Layout({ children }) {
   return (
-    <StoreProvider>{children}</StoreProvider>
+    <StoreProvider>
+      {children}
+      <Toaster
+        position='top-right'
+        toastOptions={{ style: { fontSize: "1rem" } }}
+      />
+    </StoreProvider>
   )
 }
