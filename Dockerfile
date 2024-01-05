1ba2094bb3cd1853d6083df9311a731cf7eb9a1d
huanghanzhilian <1319639755@qq.com>
2024-01-05 16:41:16 +0800

    feat: modify docker

--- Dockerfile
+++ Dockerfile
@@ -9,25 +9,13 @@ WORKDIR /app
 # 设置环境变量
 ENV MONGODB_URL "mongodb://db:27017/choiceshop"
 
-# wait 工具
-ENV WAIT_VERSION 2.7.2
-ADD https://github.com/ufoscout/docker-compose-wait/releases/download/$WAIT_VERSION/wait /wait
-RUN chmod +x /wait
-
 # 安装项目依赖
 COPY package.json /app
 RUN npm install --registry https://registry.npm.taobao.org
 COPY . /app
 
-# 监听数据是否启动
-# ENTRYPOINT ["/wait"]
-
-# 构建
-# RUN npm run build
-
 # 对外暴露端口
 EXPOSE 3000
 
 # 启动 Image 时执行命令
-CMD /wait && npm run build && npm run start
-# CMD npm run dev
+# CMD npm run start
--- helpers/db.js
+++ helpers/db.js
@@ -1,18 +1,14 @@
 import mongoose, { connection } from 'mongoose'
 
 async function connect() {
-  console.log('process.env.MONGODB_URL', process.env.MONGODB_URL)
   if (!connection.readyState) {
     mongoose.set('strictQuery', false)
     try {
       await mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        socketTimeoutMS: 1000 * 60 * 110,
-        connectTimeoutMS: 1000 * 60 * 110,
       })
       console.log('Mongo Connected: ')
-      console.log('Mongo Connected: process.env.MONGODB_URL', process.env.MONGODB_URL)
     } catch (error) {
       console.error('Failed to connect to MongoDB:', error.message)
     }
