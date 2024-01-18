fd87ad2569060f3a83fb92b8414871acfe43206a
huanghanzhilian <1319639755@qq.com>
2024-01-18 17:30:31 +0800

    fix: users models

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,22 @@
-# 加载基础镜像
 FROM node:18-alpine
 
-# 创建工作目录
-#RUN rm -rf /app
 RUN mkdir /app
 WORKDIR /app
 
-# 设置环境变量
-ENV MONGODB_URL "mongodb://db:27017/choiceshop"
-
-# 安装项目依赖
 COPY package.json /app
-# RUN npm install -g next --registry https://registry.npm.taobao.org
+
+RUN npm install -g next --registry https://registry.npm.taobao.org
 RUN npm install --registry https://registry.npm.taobao.org
+
 COPY . /app
+
+RUN mkdir -p /app/.next/cache/images
+VOLUME /app/.next/cache/images
 
 RUN npm run build
 
-# 对外暴露端口
+ENV MONGODB_URL "mongodb://db:27017/choiceshop"
+
 EXPOSE 3000
 
 # 启动 Image 时执行命令
--- models/Users.js
+++ models/Users.js
@@ -1,58 +1,61 @@
 import mongoose from 'mongoose'
 import basePlugin from './base_model'
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, default: 'user' },
-  root: { type: Boolean, default: false },
-  address: {
-    type: {
-      postalCode: {
-        type: String,
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: 'user' },
+    root: { type: Boolean, default: false },
+    address: {
+      type: {
+        postalCode: {
+          type: String,
+        },
+        street: {
+          type: String,
+        },
+        area: {
+          code: {
+            type: String,
+          },
+          name: {
+            type: String,
+          },
+          cityCode: {
+            type: String,
+          },
+          provinceCode: {
+            type: String,
+          },
+        },
+        city: {
+          code: {
+            type: String,
+          },
+          name: {
+            type: String,
+          },
+          provinceCode: {
+            type: String,
+          },
+        },
+        province: {
+          code: {
+            type: String,
+          },
+          name: {
+            type: String,
+          },
+        },
       },
-      street: {
-        type: String,
-      },
-      area: {
-        code: {
-          type: String,
-        },
-        name: {
-          type: String,
-        },
-        cityCode: {
-          type: String,
-        },
-        provinceCode: {
-          type: String,
-        },
-      },
-      city: {
-        code: {
-          type: String,
-        },
-        name: {
-          type: String,
-        },
-        provinceCode: {
-          type: String,
-        },
-      },
-      province: {
-        code: {
-          type: String,
-        },
-        name: {
-          type: String,
-        },
-      },
+      required: false,
     },
-    required: false,
+    mobile: { type: String },
   },
-  mobile: { type: String },
-})
+  { timestamps: true }
+)
 
 UserSchema.plugin(basePlugin)
 const User = mongoose.models.user || mongoose.model('user', UserSchema)
