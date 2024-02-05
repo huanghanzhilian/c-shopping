6a0130fc05850c4624e56e56aa49a10960045f5c
iGmainC <48195631+iGmainC@users.noreply.github.com>
2024-02-05 17:28:27 +0800

    购物车里不存在东西时显示0

--- components/cart/CartBadge.jsx
+++ components/cart/CartBadge.jsx
@@ -12,7 +12,7 @@ export default function CartBadge() {
   return (
     <div className="relative">
       <span className="absolute outline outline-2 bottom-3.5 left-5 bg-red-500 rounded-md w-5 h-5 p-0.5 text-center text-xs text-white">
-        {formatNumber(totalItems)}
+        {formatNumber(totalItems, '0')}
       </span>
 
       <Icons.Cart className="icon h-7 w-7" />
